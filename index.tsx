dde6d6379080de0f4ca6f561d91a17ef5d1dfc07
Reese Lee <48657837+reese-lee@users.noreply.github.com>
2019-09-06 16:48:42 -0700

    add index file

---
+++ index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createHistory from 'history/createBrowserHistory';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
+import App from 'components/App';
+import reducers from 'state/index';
+import sagas from 'sagas/index';
+
+const sagaMiddleware = createSagaMiddleware();
+const history = createHistory();
+
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer,
+  }),
+  composeWithDevTools(applyMiddleware(routerMiddleware(history), sagaMiddleware)),
+);
+
+sagaMiddleware.run(sagas);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('app'),
+);
